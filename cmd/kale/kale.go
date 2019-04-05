6b7951c0e4b680538613ae617d6b42012f0424af
Niklas Voss <niklas.voss@gmail.com>
2019-04-05 22:45:59 +0200

    Setup command for target and test flag parsing.

--- cmd/kale/kale.go
+++ cmd/kale/kale.go
@@ -2,26 +2,84 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/trevex/kale/pkg/builtin"
 	"github.com/trevex/kale/pkg/global"
+	"github.com/trevex/kale/pkg/module"
+	"go.starlark.net/starlark"
 )
 
+const data = `
+print(greeting + ", world")
+
+squares = [x*x for x in range(10)]
+
+bar = require("foo")
+baz = bar.sayHello()
+print(baz)
+
+def deploy(params):
+	print(params)
+
+target(deploy)
+`
+
 func main() {
-	rootCmd := &cobra.Command{
-		Use: "kale [flags] [target]",
-		// SilenceUsage: true,
-		Short: "",
-		Long:  ``,
+	cmd := &cobra.Command{
+		Use:          "kale [flags] [target]",
+		SilenceUsage: true,
+		Short:        "",
+		Long:         ``,
 	}
 	// Persistent flags
-	flags := rootCmd.PersistentFlags()
+	flags := cmd.PersistentFlags()
 	flags.BoolVar(&global.DryRun, "dry-run", global.DryRun, "Whether to run target without introducing changes (default false)")
 	flags.StringVar(&global.Namespace, "namespace", global.Namespace, "Which namespace the target should operate in (default \"\")")
 	flags.StringVar(&global.Release, "release", global.Release, "How the artifacts of the target should be named (default \"\")")
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
+	// Parse flags once beforehand to make them available via global variables
+	cmd.ParseFlags(os.Args)
+	// Setup modules
+	mgr := module.NewManager()
+	mgr.Set("foo", func(params *starlark.Dict) (starlark.Value, error) {
+		foo := &module.Module{}
+		foo.SetKeyFunc(starlark.String("sayHello"), func(_ *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+			fmt.Println("Hello, go")
+			return starlark.String("Hello, starlark"), nil
+		})
+		return foo, nil
+	})
+	// Setup thread and environment
+	thread := &starlark.Thread{
+		Name:  "kale",
+		Print: func(_ *starlark.Thread, msg string) { fmt.Println(msg) },
+	}
+	predeclared := starlark.StringDict{
+		"greeting": starlark.String("hello"),
+		"require":  builtin.Require(mgr),
+		"target":   builtin.Target(cmd),
+	}
+
+	// Run the script
+	globals, err := starlark.ExecFile(thread, "apparent/filename.star", data, predeclared)
+	if err != nil {
+		if evalErr, ok := err.(*starlark.EvalError); ok {
+			log.Fatal(evalErr.Backtrace())
+		}
+		log.Fatal(err)
+	}
+
+	// Print the global environment.
+	fmt.Println("\nGlobals:")
+	for _, name := range globals.Keys() {
+		v := globals[name]
+		fmt.Printf("%s (%s) = %s\n", name, v.Type(), v.String())
+	}
+	// Run the command
+	if err := cmd.Execute(); err != nil {
+		log.Fatal(err)
 		os.Exit(1)
 	}
 }
--- pkg/builtin/target.go
+++ pkg/builtin/target.go
@@ -3,20 +3,37 @@ package builtin
 import (
 	"fmt"
 
+	"github.com/spf13/cobra"
 	"go.starlark.net/starlark"
 )
 
-func target(_ *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	var (
-		targetImpl   starlark.Callable
-		paramsSchema starlark.Dict
-	)
-	if err := starlark.UnpackArgs("target", args, kwargs, "func", &targetImpl, "params?", &paramsSchema); err != nil {
-		return nil, err
-	}
-	fmt.Println(targetImpl.Name())
-	// TODO: do something
-	return starlark.None, nil
-}
+func Target(rootCmd *cobra.Command) starlark.Value {
+	return starlark.NewBuiltin("target", func(_ *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+		var (
+			targetImpl   starlark.Callable
+			paramsSchema starlark.Dict
+		)
+		if err := starlark.UnpackArgs("target", args, kwargs, "func", &targetImpl, "params?", &paramsSchema); err != nil {
+			return nil, err
+		}
+		targetName := targetImpl.Name()
+		// Create command
+		cmd := &cobra.Command{
+			Use:   fmt.Sprintf("%s [flags]", targetName),
+			Short: "TODO",
+			Long:  `TODO`,
+			RunE: func(_ *cobra.Command, args []string) error {
+				fmt.Println("TEST")
+				return nil
+			},
+		}
+		rootCmd.AddCommand(cmd)
+		// Setup flags
+		flags := cmd.Flags()
+		test := false
+		flags.BoolVar(&test, "test", false, "foo")
+		// TODO: do something
+		return starlark.None, nil
 
-var Target = starlark.NewBuiltin("target", target)
+	})
+}
