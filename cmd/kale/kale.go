21d4f1e068c07748b56f18ed4a86d82a3c20f755
Niklas Voss <niklas.voss@gmail.com>
2019-04-22 20:23:53 +0200

    Add newlines to avoid license ending up in godocs

--- cmd/kale/kale.go
+++ cmd/kale/kale.go
@@ -14,9 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-/*
-Kale, a kubernetes building, scripting and packaging tool.
-*/
 package main
 
 import (
